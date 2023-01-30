7a4ca45d98ecf843c747b0345ce7f024e137d2aa
Charlie Hengemihle <charliehengemihle@gmail.com>
2023-01-30 11:07:09 -0700

    remix

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,11 @@
-export default function Animal() {
-  return <></>;
+import './Animal.css';
+
+export default function Animal(props) {
+  return (
+    <div className="animal" style={{ top: props.top, left: props.left }}>
+      <img alt={props.name} src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
+      <p>{props.name}</p>
+      <p>{props.says}</p>
+    </div>
+  );
 }
